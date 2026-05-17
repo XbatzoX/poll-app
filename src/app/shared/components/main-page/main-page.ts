08f6ac63c085059f244ac1622e5146d524659a5d
BEN <benbasic@outlook.de>
2026-05-17 11:05:13 +0200

    added main-surveys component

--- src/app/shared/components/main-page/main-page.ts
+++ src/app/shared/components/main-page/main-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MainCaption } from '../main-caption/main-caption';
+import { MainSurveys } from "../main-surveys/main-surveys";
 
 @Component({
   selector: 'app-main-page',
-  imports: [MainCaption],
+  imports: [MainCaption, MainSurveys],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
---
+++ src/app/shared/components/main-surveys/main-surveys.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-surveys',
+  imports: [],
+  templateUrl: './main-surveys.html',
+  styleUrl: './main-surveys.scss',
+})
+export class MainSurveys {}
