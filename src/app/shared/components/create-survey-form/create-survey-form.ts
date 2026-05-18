6b37f7962360d5f0a14739b70e817e79c99beea0
BEN <benbasic@outlook.de>
2026-05-18 17:09:09 +0200

    worked on new survey form

--- src/app/shared/components/create-survey-form/create-survey-form.ts
+++ src/app/shared/components/create-survey-form/create-survey-form.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './create-survey-form.html',
   styleUrl: './create-survey-form.scss',
 })
-export class CreateSurveyForm {}
+export class CreateSurveyForm {
+  isHovered: boolean;
+
+  constructor(){
+    this.isHovered = false;
+  }
+
+  changeDeleteIcon():string{
+    if(this.isHovered){
+      return 'assets/icons/delete_hover.svg';
+    }else{
+      return 'assets/icons/Delete.svg';
+    }
+  }
+}
