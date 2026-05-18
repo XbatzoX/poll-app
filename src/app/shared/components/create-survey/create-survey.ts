f42c615ab5fb70c17c81e88504cc3ee0d9e5ec72
BEN <benbasic@outlook.de>
2026-05-18 14:35:11 +0200

    added child component survay form to create survey parent component

--- src/app/shared/components/create-survey/create-survey.ts
+++ src/app/shared/components/create-survey/create-survey.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { CreateSurveyForm } from '../create-survey-form/create-survey-form';
 
 @Component({
   selector: 'app-create-survey',
-  imports: [],
+  imports: [CreateSurveyForm],
   templateUrl: './create-survey.html',
   styleUrl: './create-survey.scss',
 })
