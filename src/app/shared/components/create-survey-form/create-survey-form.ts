7c644ce7949233a72f2dd2e1de5141db66ec6079
BEN <benbasic@outlook.de>
2026-05-18 14:30:04 +0200

    added createSurvey-form component

---
+++ src/app/shared/components/create-survey-form/create-survey-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-survey-form',
+  imports: [],
+  templateUrl: './create-survey-form.html',
+  styleUrl: './create-survey-form.scss',
+})
+export class CreateSurveyForm {}
