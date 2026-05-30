95084b109484ad10094b1eae5a59cab50505bea0
BEN <benbasic@outlook.de>
2026-05-30 17:53:57 +0200

    craeted dialog after succesful published survey

--- src/app/shared/components/create-survey-dialog/create-survey-dialog.ts
+++ src/app/shared/components/create-survey-dialog/create-survey-dialog.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, ElementRef, model, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-create-survey-dialog',
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './create-survey-dialog.html',
   styleUrl: './create-survey-dialog.scss',
 })
-export class CreateSurveyDialog {}
+export class CreateSurveyDialog {
+  isDialogOpen = model<boolean>(false);
+  dialogRef = viewChild<ElementRef<HTMLDialogElement>>('publishedDialog');
+
+  constructor(){
+    effect((onCleanup) => {
+      let dialog = this.dialogRef()?.nativeElement;
+      if(!dialog) return;
+      if(this.isDialogOpen()){
+        dialog.showModal();
+        let timer = setTimeout(() => {this.isDialogOpen.set(false);}, 2000);
+        onCleanup(() => {clearTimeout(timer);});
+      }else{
+        let closeTimer = setTimeout(()=>{dialog.close();}, 500);
+        onCleanup(() => {clearTimeout(closeTimer);});
+      }
+    });
+  }
+}
